35e422ba288db52a2442e514944036871cef9cfa
Konstantin <lable_01@mail.ru>
2021-07-02 07:15:33 +0300

    добавил кнопку, php doc

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -10,7 +10,7 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 ?>
 <div class="site-login">
-    <p>Если вы не администратор пройдите по <a href="../../../frontend/web">ссылке</a></p>
+    <a href="../../../frontend/web" class="btn btn-info">На главную</a>
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Please fill out the following fields to login:</p>
--- frontend/models/User.php
+++ frontend/models/User.php
@@ -8,12 +8,17 @@ use yii\db\ActiveRecord;
 
 class User extends ActiveRecord
 {
-
     public static function tableName()
     {
         return '{{user}}';
     }
 
+    /**
+     * @param $date_1 string меньшая дата
+     * @param $date_2 string большая дата
+     * @param string $differenceFormat тип форматирования даты
+     * @return string разница между датами в выбранном формате
+     */
     public static function dateDifference($date_1 , $date_2 , $differenceFormat = '%y')
     {
         $datetime1 = date_create($date_1);
